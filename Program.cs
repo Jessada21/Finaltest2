f77c8b44d8a4af190a56839664392e99d01982d7
JidapaBubu <jidapa4499@gmail.com>
2023-05-15 16:47:40 +0700

    Test2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Finaltest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int Vmax = int.Parse(Console.ReadLine());
+         int Vfill = int.Parse(Console.ReadLine());
+         int Vdrink =int.Parse(Console.ReadLine());
+         int Tday = int.Parse(Console.ReadLine());
+         int Tdrink = int.Parse(Console.ReadLine());
+         int Tfill = int.Parse(Console.ReadLine());
+        }
+    }
+}
